b2fb3fd9d7a99b031f8e32ddc9fbbb3e5eaa427e
Luiz Eugênio Barbieri <luizeugeniobarbieri@gmail.com>
2020-08-16 15:17:23 -0300

    Implementation of the user service.

---
+++ src/ApiDDD.Service/Services/UserService.cs
@@ -0,0 +1,44 @@
+﻿using ApiDDD.Data.Entities;
+using ApiDDD.Domain.Interfaces;
+using ApiDDD.Domain.Interfaces.Services.User;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiDDD.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private IRepository<UserEntity> _repository;
+
+        public UserService(IRepository<UserEntity> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+
+        public async Task<UserEntity> Get(Guid id)
+        {
+            return await _repository.SelectAsync(id);
+        }
+
+        public async Task<IEnumerable<UserEntity>> GetAll()
+        {
+            return await _repository.SelectAsync();
+        }
+
+        public async Task<UserEntity> Post(UserEntity user)
+        {
+            return await _repository.InsertAsync(user);
+        }
+
+        public async Task<UserEntity> Put(UserEntity user)
+        {
+            return await _repository.UpdateAsync(user);
+        }
+    }
+}
